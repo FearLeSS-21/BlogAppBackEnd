0294cef71371bdc76a643e0fbb7924602250cb08
Zeyad Wael <Zeyad.wael.2003@gmail.com>
2024-09-11 16:06:58 +0300

    login endpoint

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -6,6 +6,8 @@ import org.example.validation.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
@@ -29,6 +31,26 @@ public class UserController {
 
         User registeredUser = userService.registerUser(user);
         return ResponseEntity.ok(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User loginRequest) {
+        String email = loginRequest.getEmail();
+        String password = loginRequest.getPassword();
+
+        User user = userService.findByEmail(email);
+        if (user == null) {
+            // Return a JSON object with an error message
+            return ResponseEntity.status(404).body(Map.of("message", "User not found"));
+        }
+
+        if (!userService.checkPassword(user, password)) {
+            // Return a JSON object with an error message
+            return ResponseEntity.status(401).body(Map.of("message", "Invalid credentials"));
+        }
+
+        // Return success response as JSON
+        return ResponseEntity.ok(Map.of("message", "Login successful"));
     }
 
 }
