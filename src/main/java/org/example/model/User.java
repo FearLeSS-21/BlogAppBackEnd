9138a5dcd73f506d853d795f0c6af41d2498fcaf
Zeyad Wael <Zeyad.wael.2003@gmail.com>
2024-09-22 16:28:25 +0300

    Added a DTO and edited user model

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,13 +1,9 @@
 package org.example.model;
 
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-
 import lombok.*;
 
 @Data
@@ -20,18 +16,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "Email is mandatory")
-    @Pattern(regexp = "^(?=.*example)([^@\\s]+)@([^@\\s]+\\.[^@\\s]+)$",
-            message = "Email must contain 'example' and have a valid format with exactly one '@' symbol.")
     private String email;
-
-
-    @NotBlank(message = "Password is mandatory")
-    @Size(min = 6, message = "Password should be at least 6 characters long")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=]).*$",
-            message = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")
     private String password;
-
-    @NotBlank(message = "Name is mandatory")
     private String name;
 }
